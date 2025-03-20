c21351d5fc386bc081b2f0655c5bc01b2eac34e4
Oleksandr Holovei <oleksandr.holovei@synotgames.com>
2025-03-20 18:37:45 +0200

    added json

---
+++ zalupa.js
@@ -0,0 +1,30 @@
+const films = {
+    "film1": {
+        "name": "zalupa konya",
+        "year": 1988,
+        "photo":"../../photo/zalupaKonya.jpg",
+        "genre": "horror"
+    },
+    "film2":{
+        "name": "zalupa konya",
+        "year": 1988,
+        "photo":"../../photo/zalupaKonya.jpg",
+        "genre": "horror"
+    },
+    "film3":{
+        "name": "zalupa konya",
+        "year": 1988,
+        "photo":"../../photo/zalupaKonya.jpg",
+        "genre": "horror"
+    },
+    "film4":{
+        "name": "zalupa konya",
+        "year": 1988,
+        "photo":"../../photo/zalupaKonya.jpg",
+        "genre": "horror"
+    }
+};
+
+for (film in films) {
+ console.log(film)
+}
